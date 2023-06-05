d530ead4d7dc50eedb7a3cae6398e0f2255f22d8
Mohamed Takrayout <mohtakra@e1r7p5.1337.ma>
2023-06-05 22:18:32 +0100

    the new v

--- libphilo.h
+++ libphilo.h
@@ -6,7 +6,7 @@
 /*   By: mohtakra <mohtakra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 16:31:00 by mohtakra          #+#    #+#             */
-/*   Updated: 2023/06/03 16:36:18 by mohtakra         ###   ########.fr       */
+/*   Updated: 2023/06/05 21:06:31 by mohtakra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <sys/time.h>
 # include <pthread.h>
 # define AVAILABLE		1
 # define NO_AVAILABLE	0
@@ -25,8 +26,9 @@
 typedef struct s_list
 {
 	int				nbr;
-	pthread_mutex_t	own_fork;
+	double			last_meal;
 	int				status;
+	pthread_mutex_t	own_fork;
 	struct s_list	*next;
 } t_list;
 
--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: mohtakra <mohtakra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 16:35:52 by mohtakra          #+#    #+#             */
-/*   Updated: 2023/06/04 18:11:25 by mohtakra         ###   ########.fr       */
+/*   Updated: 2023/06/05 22:11:48 by mohtakra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	print_lst(t_list *lst)
 {
 	t_list	*tmp;
+	int		breaker = lst->nbr;
 
 	tmp = lst;
 	printf("\n{----\n");
@@ -24,21 +25,59 @@ void	print_lst(t_list *lst)
 		printf("| mutex  = %p ",&(lst->own_fork));
 		printf("| status = %d |\n",lst->status);
 		lst = lst->next;
+		if (lst->nbr == breaker)
+			break ;
 	}
 	printf("\n----}\n");
 	lst = tmp;
 	printf("\n");
 }
 
+
+/*return the number of milisecond in a given number of seconds*/
+double	convert_sec_to_millisec(int sec)
+{
+	return ((double) sec * 1000);
+}
+
+/*return the number of milisecond in a given number of microsecond*/
+double	convert_microsec_to_millisec(int microsec)
+{
+	return ((double) microsec / 1000);
+}
+
 void	*sleep_think_eat(void *args)
 {
-	t_list	*lst = (t_list*)args;
+	t_list	*lst;
+	struct timeval t;
 
-	printf("\n| nbr    = %d ",lst->nbr);
-	printf("| mutex  = %p ",&(lst->own_fork));
-	printf("| status = %d |\n",lst->status);
-	// write(1, "here is the function pointer ", 31);
-	// printf("  %d \n",ft_lstsize(lst));
+	lst = (t_list*)args;
+	gettimeofday(&t, NULL);
+	// printf("here is the test for the use of sec to milli : %ld s = %f millisec\n",t.tv_sec, convert_sec_to_millisec(t.tv_sec));
+	// printf("here is the test for the use of micro to milli : %d us = %f millisec\n",t.tv_usec, convert_microsec_to_millisec(t.tv_usec));
+	lst->last_meal = convert_sec_to_millisec(t.tv_sec) + convert_microsec_to_millisec(t.tv_usec);
+	// printf("the new last meal is %f \n",lst->last_meal);
+	// sleep(2);
+	while (convert_sec_to_millisec(t.tv_sec) + convert_microsec_to_millisec(t.tv_usec) - lst->last_meal <= 1 )
+	{
+		pthread_mutex_lock(&lst->own_fork);
+		pthread_mutex_lock(&lst->next->own_fork);
+		printf("\nthe philo %d is START eating usec = %f currtime = %d\n",lst->nbr,lst->last_meal, t.tv_usec);
+		usleep(200 / 1000);
+		gettimeofday(&t, NULL);
+		lst->last_meal = convert_sec_to_millisec(t.tv_sec) + convert_microsec_to_millisec(t.tv_usec);
+		printf("\nthe philo %d is END eating usec = %f currtime = %d\n",lst->nbr,lst->last_meal, t.tv_usec);
+		pthread_mutex_unlock(&lst->next->own_fork);
+		pthread_mutex_unlock(&lst->own_fork);
+		printf("\nthe philo %d is START sleeping usec = %f currtime = %d\n",lst->nbr,lst->last_meal, t.tv_usec);
+		usleep(1 / 1000);
+		printf("\nthe philo %d is END sleeping usec = %f currtime = %d\n",lst->nbr,lst->last_meal, t.tv_usec);
+		printf("\nthe philo %d is START thinking usec = %f currtime = %d\n",lst->nbr,lst->last_meal, t.tv_usec);
+		usleep(1 / 1000);
+		printf("\nthe philo %d is END thinking usec = %f currtime = %d\n",lst->nbr,lst->last_meal, t.tv_usec);
+		gettimeofday(&t, NULL);
+	}
+	printf("\nthe philo %d is daying usec = %f currtime = %d\n",lst->nbr,lst->last_meal, t.tv_usec);
 	return (NULL);
 }
 
@@ -48,14 +87,14 @@ int	main(int argc, char **argv)
 	int				i;
 	pthread_mutex_t	mutex[4];
 	t_list			*lst;
-	
+
 	i = 0;
 	lst = NULL;
 	(void) argc;
 	(void) argv;
 	// printf("the address of the i = %p\n",&i);
 	//here check the parsing is ok or not
-	while (i < 4)
+	while (i < 5)
 	{
 		ft_lstadd_back(&lst, ft_lstnew(i + 1, mutex[i]));
 		if (pthread_create(&thread[i], NULL, &sleep_think_eat, ft_lstlast(lst)) != 0)
@@ -65,19 +104,19 @@ int	main(int argc, char **argv)
 		}
 		i++;
 	}
-	// ft_lstlast(lst)->next = lst;
+	ft_lstlast(lst)->next = lst;
 	i = 0;
-	while (i < 4)
+	while (i < 5)
 	{
-		printf("the %d thread begin \n", i);
+		// printf("the %d thread begin \n", i);
 		if (pthread_join(thread[i], NULL) != 0)
 		{
 			printf("there is an error while joining the thread nbr %d\n",i+1);
 			return (1);
 		}
-		printf("the %d thread end \n", i);
+		// printf("the %d thread end \n", i);
 		i++;
 	}
-	printf("\n************here is the list***************\n");
-	print_lst(lst);
+	// printf("\n************here is the list***************\n");
+	// print_lst(lst);
 }
